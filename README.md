7c0bf7427f5417dd18e32c5bc7fd10f6a03a9853
Martin Helmich <m.helmich@mittwald.de>
2024-10-29 13:12:22 +0100

    Configure PHPStan+PHPUnit

---
+++ README.md
@@ -0,0 +1,19 @@
+# mStudio extension authentication services
+
+This library contains PHP classes for authenticating users in mStudio extensions.[^1]
+
+> [!TIP]
+> This library *only* contains the service classes for authenticating requests and users
+> in mStudio extensions. If you are using [Symfony](http://symfony.com),
+> we recommend using the [mstudio-ext-bundle Symfony Bundle](https://github.com/mittwald/mstudio-ext-bundle),
+> instead.
+
+[^1]: https://developer.mittwald.de/docs/v2/contribution/reference/webhooks/
+
+## Installation
+
+Install using Composer:
+
+```
+$ composer require mittwald/mstudio-ext-auth
+```
---
+++ tests/AuthenticationServiceTest.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Mittwald\MStudio\Authentication;
+
+use DateTime;
+use Mittwald\ApiClient\Client\UntypedResponse;
+use Mittwald\ApiClient\Error\UnexpectedResponseException;
+use Mittwald\ApiClient\Generated\V2\Clients\User\AuthenticateWithAccessTokenRetrievalKey\AuthenticateWithAccessTokenRetrievalKeyOKResponse;
+use Mittwald\ApiClient\Generated\V2\Clients\User\AuthenticateWithAccessTokenRetrievalKey\AuthenticateWithAccessTokenRetrievalKeyOKResponseBody;
+use Mittwald\ApiClient\Generated\V2\Clients\User\UserClient;
+use Mittwald\ApiClient\MittwaldAPIV2Client;
+use Nyholm\Psr7\Response;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+use Psr\Log\NullLogger;
+use function PHPUnit\Framework\any;
+use function PHPUnit\Framework\assertThat;
+use function PHPUnit\Framework\equalTo;
+use function PHPUnit\Framework\once;
+
+#[CoversClass(AuthenticationService::class)]
+class AuthenticationServiceTest extends TestCase
+{
+    #[Test]
+    public function authenticateUsesRetrievalKey(): void
+    {
+        $userID       = "USER_ID";
+        $retrievalKey = base64_encode(random_bytes(16));
+        $accessToken  = base64_encode(random_bytes(16));
+        $refreshToken = base64_encode(random_bytes(16));
+        $expiration = new DateTime("now + 3 days");
+
+        $response = new AuthenticateWithAccessTokenRetrievalKeyOKResponse(
+            new AuthenticateWithAccessTokenRetrievalKeyOKResponseBody(
+                token: $accessToken,
+                refreshToken: $refreshToken,
+                expiresAt: $expiration,
+            ),
+        );
+
+        $userClient = $this->getMockBuilder(UserClient::class)->getMock();
+        $userClient->expects(once())->method('authenticateWithAccessTokenRetrievalKey')->willReturn($response);
+
+        $client = $this
+            ->getMockBuilder(MittwaldAPIV2Client::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $client
+            ->expects(any())
+            ->method('user')
+            ->willReturn($userClient);
+
+        $sut = new AuthenticationService($client, new NullLogger());
+        $token = $sut->authenticate($userID, $retrievalKey);
+
+        assertThat($token->getAccessToken(), equalTo($accessToken));
+        assertThat($token->getRefreshToken(), equalTo($refreshToken));
+        assertThat($token->getExpiresAt(), equalTo($expiration));
+    }
+
+    #[Test]
+    public function throwsAuthenticationErrorOnUnexpectedResponse(): void
+    {
+        $userID       = "USER_ID";
+        $retrievalKey = base64_encode(random_bytes(16));
+
+        $userClient = $this->getMockBuilder(UserClient::class)->getMock();
+        $userClient
+            ->expects(once())
+            ->method('authenticateWithAccessTokenRetrievalKey')
+            ->willThrowException(new UnexpectedResponseException(new UntypedResponse("noope", new Response(403))));
+
+        $client = $this
+            ->getMockBuilder(MittwaldAPIV2Client::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $client
+            ->expects(any())
+            ->method('user')
+            ->willReturn($userClient);
+
+        $this->expectException(AuthenticationError::class);
+
+        $sut = new AuthenticationService($client, new NullLogger());
+        $sut->authenticate($userID, $retrievalKey);
+    }
+}
